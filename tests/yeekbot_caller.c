1dbe1020030fb892ccc375209f85a46cc951fe02
Bence Danko <bencejdanko@gmail.com>
2026-09-06 21:57:46 -0700

    test: revision C

--- tests/yeekbot_caller.c
+++ tests/yeekbot_caller.c
@@ -1,11 +1,8 @@
 #include <stdint.h>
 #include "yeekbot_smoke.h"
 
-/* revision B - bounds the length */
+/* revision C: reintroduce an unbounded length */
 void collect_window(const uint8_t *adc, uint32_t adc_len)
 {
-    if (adc_len > SENSOR_BUF_LEN) {
-        adc_len = SENSOR_BUF_LEN;
-    }
     ingest_samples(adc, adc_len);
 }
