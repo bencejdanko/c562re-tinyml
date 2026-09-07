c949ea5ad1651767b380113eecf3fb65bd2740da
Bence Danko <bencejdanko@gmail.com>
2026-09-06 21:43:23 -0700

    test: revision B supersedes A

--- tests/yeekbot_caller.c
+++ tests/yeekbot_caller.c
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include "yeekbot_smoke.h"
 
-/* revision A */
+/* revision B - bounds the length */
 void collect_window(const uint8_t *adc, uint32_t adc_len)
 {
+    if (adc_len > SENSOR_BUF_LEN) {
+        adc_len = SENSOR_BUF_LEN;
+    }
     ingest_samples(adc, adc_len);
 }
